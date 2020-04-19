3c341dc5062286b3848d995d08991ab8b776b7a3
alasmirnov <alasmirnov@outlook.com>
2020-04-19 07:15:14 +0300

    Refactoring. http requests have been extracted in method

--- Service/Actions/ActionsBot.cs
+++ Service/Actions/ActionsBot.cs
@@ -26,27 +26,16 @@ namespace Service.Actions
 
             var result = new List<ActionCommand>();
 
-            using (var client = new HttpClient())
+            var response = await InternalGetAsync<GetUpdatesResponse>(url, cancellationToken);
+
+            foreach (UpdateDto update in response.Updates)
             {
-                HttpResponseMessage responseMessage = await client.GetAsync(url, cancellationToken);
-
-                responseMessage.EnsureSuccessStatusCode();
-
-                string responseJson = await responseMessage.Content.ReadAsStringAsync();
-
-                var response = JsonSerializer.Deserialize<GetUpdatesResponse>(responseJson);
-                
-                response.CheckException();
-
-                foreach (UpdateDto update in response.Updates)
-                {
-                    var user = new User(update.Message.User.Id, update.Message.User.Name);
+                var user = new User(update.Message.User.Id, update.Message.User.Name);
                     
-                    result.Add(new ActionCommand(update.Message.Text, user, user.Id));
+                result.Add(new ActionCommand(update.Message.Text, user, user.Id));
 
-                    if (update.Id > _offset)
-                        _offset = update.Id;
-                }
+                if (update.Id > _offset)
+                    _offset = update.Id;
             }
 
             return result;
@@ -61,23 +50,29 @@ namespace Service.Actions
             };
             
             string url = UrlBuilder.BuildUrl("sendMessage", _token, parameters);
-
-            using (var client = new HttpClient())
-            {
-                HttpResponseMessage responseMessage = await client.GetAsync(url, cancellationToken);
-
-                string resp = await responseMessage.Content.ReadAsStringAsync();
-
-                var telegramResponse = JsonSerializer.Deserialize<SendMessageResponse>(resp);
-                
-                telegramResponse.CheckException();
-            }
+            
+            var telegramResponse = await InternalGetAsync<SendMessageResponse>(url, cancellationToken);
         }
         
         public ActionsBot(string token)
         {
             _offset = 0;
             _token = token;
+        }
+        
+        private async Task<T> InternalGetAsync<T>(string url, CancellationToken cancellationToken) where T : TelegramResponseBase
+        {
+            using var client = new HttpClient();
+            
+            HttpResponseMessage httpResponse = await client.GetAsync(url, cancellationToken);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = JsonSerializer.Deserialize<T>(await httpResponse.Content.ReadAsStringAsync());
+                
+            response.CheckException();
+
+            return response;
         }
     }
 }
